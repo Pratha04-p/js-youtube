aba9e0b71bfb3531eebcf0acc54a82ae76dc9703
Pratha04-p <pandeypratha86@gmail.com>
2026-04-17 13:01:02 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail = "pratha@gmail.com"
+var accountPassword = "12345"
+accountCity = "mumbai";
+let accountState
+
+//accountId = 2 //not allowed to change const
+
+accountEmail = "pp@pp.com"
+accountPassword = "212121211"
+accountCity = "delhi"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
