e288100a6168dfc325477162ee29c91efb0f3896
Pratha04-p <pandeypratha86@gmail.com>
2026-04-17 13:01:20 +0000

    variables

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict" //treat all js code as newer version
+
+//alert(3+3) //we're using nodejs, not browser used to show popup to user/browser with ok button
+
+console.log(3+3); console.log("pratha") //better ki next line mein ho for better code readability
+
+
+//primitive datatypes
+let name = "pratha" //string dt
+let age = 21 //number dt
+let isLoggedIn = false //boolean dt
+//null //standalone value also can be alloted to tell variable is null not undefined
+//undefined //value not assigned yet
+//symbol //unique
+
+//objects 
+
+console.log(typeof null);
+console.log(undefined);
+
