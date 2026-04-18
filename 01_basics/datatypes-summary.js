323fb457f57603565fd8db9bd2e205b26d956906
Pratha04-p <pandeypratha86@gmail.com>
2026-04-18 12:08:34 +0000

    summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,20 @@
+// # primitive data types 
+/* 7 types: String, Number, Boolean, Null, Undefined, Symbol(to make any value unique), BigInt */
+
+
+//js is dynamically typed 
+ let x = 10;     // number
+//x = "hello";    // ab string
+console.log(x);
+
+
+// # reference or non primitive data type 
+/* Array, Objects, Functions*/
+
+// +++++++++++++++++++++++++++++++ //
+
+//stack and heap memory
+stack -> primitive //you get a copy
+heap -> non primitive // you get reference of original value
+
+let myYoutubeName = 
