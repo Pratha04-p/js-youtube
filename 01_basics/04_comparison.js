6abca78a22fb13a5cb9a171c6ced1429b745dc30
Pratha04-p <pandeypratha86@gmail.com>
2026-04-18 07:43:10 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,19 @@
+/*console.log(2>3);
+console.log(2<5);
+console.log(2>=2);
+console.log(2!=3);
+console.log(2 == 3);*/
+
+//console.log("2">1);
+
+/*console.log(null >0); //false
+console.log(null == 0); //false
+console.log(null >=0); //true
+
+console.log(undefined == 0); //false
+console.log(undefined > 0); //false
+console.log(undefined < 0); //true*/
+
+
+// === strict check :- check including datatype
+console.log("2" === 2);
