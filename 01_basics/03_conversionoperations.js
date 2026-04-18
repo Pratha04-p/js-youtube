1c83f383427e1e426b03a62cfdcdbd48275bdb86
Pratha04-p <pandeypratha86@gmail.com>
2026-04-18 07:12:57 +0000

    conversion and operations

--- 01_basics/03_conversionoperations.js
+++ 01_basics/03_conversionoperations.js
@@ -1,4 +1,4 @@
-let score = "33ABC" //if value is null then answer will be 0 
+/*let score = "33ABC" //if value is null then answer will be 0 
 
 
 console.log(typeof score);
@@ -17,6 +17,53 @@ console.log(typeof valueInNumber)
 
 let marks = 33//ab isko string mein badalna hai toh ek naya variable banao fir string() mein iska naam daalo bas
 
-let score = String(marks)
-console.log(score);
-console.log(typeof score)
+let scoree = String(marks)
+console.log(scoree);
+console.log(typeof scoree)
+
+ //********operations*************/
+
+let value = 7
+let negValue = -value
+//console.log(negValue);
+
+/*console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2/2);
+console.log(2**2);
+console.log(2%2);*/
+
+let str1 = "Hey"
+let str2 = " P !"
+str3 = str1 + str2
+//console.log(str3);
+//console.log(str1 + str2)
+
+/*console.log("1" + 2);
+console.log(1 + "2");
+console.log("1"+ 2 + "2");
+console.log(1 + 2 + "2");*/
+//console.log(1+2+"3"+4+5+"6"+1+2); // ek baar string aagai uske baad sab concat hi hoga
+
+/*console.log(+true) // gives 1
+console.log(+"") // gives 0    */
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+
+
+//let gameCounter = 100
+//gameCounter++
+//console.log(gameCounter);
+
+let gameCounter = 100
+++gameCounter
+console.log(gameCounter);
+
+
+
+
+
+
+
