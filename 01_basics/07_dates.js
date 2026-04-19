4c63957f1d01ef6c042c8721dd1aea591fd4e43c
Pratha04-p <pandeypratha86@gmail.com>
2026-04-19 14:17:38 +0000

    dates

--- 01_basics/07_dates.js
+++ 01_basics/07_dates.js
@@ -1,24 +1,24 @@
-/*let myDate = new Date()
+let myDate = new Date()
 console.log(myDate.toString())
 console.log(myDate.toDateString());
 console.log(myDate.toISOString());
 console.log(myDate.toJSON())
 console.log(myDate.toLocaleDateString());
 console.log(myDate.toLocaleString());
-console.log(typeof myDate);*/
+console.log(typeof myDate);
 
 //specific date
-//let myCreatedDate = new Date(2023, 0, 23, 15, 40)
-//console.log(myCreatedDate.toDateString())
-//console.log(myCreatedDate.toLocaleString())
+let myCreatedDate1 = new Date(2023, 0, 23, 15, 40)
+console.log(myCreatedDate.toDateString())
+console.log(myCreatedDate.toLocaleString())
 let myCreatedDate = new Date("01-17-2004")
-//console.log(myCreatedDate.toLocaleString());
+console.log(myCreatedDate.toLocaleString());
 
 let myTimeStamp = Date.now()
-/*console.log(myTimeStamp);
+console.log(myTimeStamp);
 console.log(myCreatedDate.getTime());
 console.log(Date.now()/1000);
-console.log(Math.floor(Date.now()/1000))*/
+console.log(Math.floor(Date.now()/1000))
 
 let newDate = new Date()
 console.log(newDate.getMonth())
